6b4c9b6a41cfb88c0c1574f221f31902535c921e
maforget <11904426+maforget@users.noreply.github.com>
2024-07-21 02:44:02 -0400

    Removed unneeded projects

--- Nuget/nuget_pack.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-nuget pack PDFium.LinuxV2.nuspec
-nuget pack PDFium.WindowsV2.nuspec
-nuget pack PDFiumSharpV2.nuspec
-nuget pack PDFiumSharp.GdiPlusV2.nuspec
-nuget pack PDFiumSharp.WpfV2.nuspec
-nuget pack PDFiumSharp.XwtV2.nuspec
-pause
